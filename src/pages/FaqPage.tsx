28fa182110377e2d33e5879f0b24e73d4ff0788c
Jay Kumar Gupta <jayesh.gupta3224@gmail.com>
2025-05-23 13:26:33 +0530

    faq completed

--- src/pages/FaqPage.tsx
+++ src/pages/FaqPage.tsx
@@ -1,12 +1,13 @@
-import Faq from '@/components/Faq/Faq'
-import React from 'react'
+import Faq from "@/components/Faq/Faq";
+import React from "react";
 
 function FaqPage() {
   return (
-      <div className='flex items-center justify-center pt-12 pb-24'>
-          <Faq/>
+    <div className="flex items-center justify-center pt-12 pb-24">
+
+      <Faq />
     </div>
-  )
+  );
 }
 
-export default FaqPage
+export default FaqPage;
